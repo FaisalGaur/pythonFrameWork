4e33862c0d46d8c7fb2a3b631d8dcd3f1ea14256
faisal07ali@gmail.com <ultrama7>
2020-04-22 10:22:47 +0530

    updated project and did test run

--- pythonAppsAuto/run_parallel.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-pytest -n 5 --html=Reports/parallel_test_report.html --css=Reports/assets/custom.css
-cd Reports
-parallel_test_report.html
-PAUSE
--- pythonAppsAuto/run_suite.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-python suite.py
-cd Reports
-Test_Suite_Report.html
-PAUSE
